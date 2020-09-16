813456f0754a15cbbcc62dbcef5bd6824ee9f645
Phillipe Roger Souza <phillipe.souza@rte.com.br>
2020-09-16 11:46:15 -0300

    COnfigurando formik

--- front-end/src/routes.js
+++ front-end/src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import QrCode from './components/qr-code';
 import {
-  BrowserRouter as Router,
+  BrowserRouter,
   Switch,
   Route,
   Link
@@ -9,51 +9,24 @@ import {
 
 import Global from './styles/global';
 import Home from './components/home'
-
-const lorem =
-  "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, sed iure blanditiis voluptatum nulla quidem minus quam tempora obcaecati necessitatibus inventore! Vitae totam quam pariatur facilis fugit maxime adipisci eaque.";
-
-const data = [
-  {
-    id: Math.random(),
-    title: "Box titulo 1",
-    text: lorem,
-    bgColor: "#D5CAFA"
-  },
-  {
-    id: Math.random(),
-    title: "Box titulo 2",
-    text: lorem,
-    bgColor: "#EDA9A9"
-  },
-  {
-    id: Math.random(),
-    title: "Box titulo 3",
-    text: lorem,
-    bgColor: "#F2EE8D"
-  },
-  {
-    id: Math.random(),
-    title: "Box titulo 4",
-    text: lorem,
-    bgColor: "#9FEACD"
-  }
-];
+import RegisterMessage from './components/register-message-form';
 
 export default function Routes() {
-    return (<Router>
+    return (<BrowserRouter>
         <div>
             <ul>
                 <Link to="/">Home</Link>
                 <Link to="/qrcode">QR Code</Link>
+                <Link to="/cadastrar">Comandos</Link>
             </ul>
         </div>
     
         <hr />
         <Switch>
-            <Route exatc path="/"><Home boxData={data} /></Route>
-            <Route exact path="/qrcode"><QrCode /> </Route>
+            <Route exact={true} path="/" component={Home}></Route>
+            <Route  path="/qrcode" component={QrCode}></Route>
+            <Route  path="/cadastrar" component={RegisterMessage}></Route>
         </Switch>
-    </Router>
+    </BrowserRouter>
     );
 }
